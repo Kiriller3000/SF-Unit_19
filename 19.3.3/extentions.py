5b158287933dd90c5b21a4babcebc1199650bf73
Kirill <134620940+Kiriller3000@users.noreply.github.com>
2023-06-23 01:56:42 +0500

    Update extentions.py

--- 19.3.3/extentions.py
+++ 19.3.3/extentions.py
@@ -13,3 +13,15 @@ def got_response(url: str, method: str, **kwargs):
         print('Не допустимый метод')
     return print('\n Статус код: ', response.status_code,'\n',f'Ответ на {method} запрос: ', response.json())
 
+data = [
+  {
+    "id": 0,
+    "username": "Name}",
+    "firstName": "FName",
+    "lastName": "LName",
+    "email": "wefw@sdvs.tk",
+    "password": "passw0rd",
+    "phone": "+1(156)651-651-651",
+    "userStatus": 0
+  }
+]
