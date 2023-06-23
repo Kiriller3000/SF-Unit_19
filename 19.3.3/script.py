78e13a5fda81e22b9c0cd76c3810b74b6c873cd1
Kirill <134620940+Kiriller3000@users.noreply.github.com>
2023-06-23 12:44:45 +0500

    Update script.py

--- 19.3.3/script.py
+++ 19.3.3/script.py
@@ -11,12 +11,12 @@ got_response(url, 'get', headers=get_headers, params=params)
 # POST RESPONSE
 url = 'https://petstore.swagger.io/v2/user/createWithList'
 post_headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
-username = data[0]['username']
 
 got_response(url, 'post', headers=post_headers, json=data)
 
 
 # ANOTHER GET RESPONSE
+username = data[0]['username']
 got_response(f'https://petstore.swagger.io/v2/user/{username}','get', headers=get_headers)
 
 
