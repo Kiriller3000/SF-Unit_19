ef0d07566cfce7cefe4e02e92b9ec17f63535505
Kirill <134620940+Kiriller3000@users.noreply.github.com>
2023-06-23 12:51:31 +0500

    Update script.py

--- 19.3.3/script.py
+++ 19.3.3/script.py
@@ -1,23 +1,24 @@
 from extentions import got_response, data
 
+GET_HEADERS = {'accept': 'application/json'}
+POST_HEADERS = {'accept': 'application/json', 'Content-Type': 'application/json'}
+
 # GET RESPONSE
-get_headers = {'accept': 'application/json'}
 params = {'status': 'sold'}
 url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=sold'
 
-got_response(url, 'get', headers=get_headers, params=params)
+got_response(url, 'get', headers=GET_HEADERS, params=params)
 
 
 # POST RESPONSE
 url = 'https://petstore.swagger.io/v2/user/createWithList'
-post_headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
 
-got_response(url, 'post', headers=post_headers, json=data)
+got_response(url, 'post', headers=POST_HEADERS, json=data)
 
 
 # ANOTHER GET RESPONSE
 username = data[0]['username']
-got_response(f'https://petstore.swagger.io/v2/user/{username}','get', headers=get_headers)
+got_response(f'https://petstore.swagger.io/v2/user/{username}','get', headers=GET_HEADERS)
 
 
 # PUT RESPONSE
@@ -25,14 +26,14 @@ url = f'https://petstore.swagger.io/v2/user/{username}'
 data = data[0]
 data['phone'] = '+7(985)116-79-61656'
 
-got_response(url, 'put', headers=post_headers, json=data)
+got_response(url, 'put', headers=POST_HEADERS, json=data)
 
 
 # AGAIN GET RESPONSE
-got_response(f'https://petstore.swagger.io/v2/user/{username}','get', headers=get_headers)
+got_response(f'https://petstore.swagger.io/v2/user/{username}','get', headers=GET_HEADERS)
 
 
 # DELETE RESPONSE
 url = f'https://petstore.swagger.io/v2/user/{username}'
 
-got_response(url, 'delete', headers=get_headers)
+got_response(url, 'delete', headers=GET_HEADERS)
